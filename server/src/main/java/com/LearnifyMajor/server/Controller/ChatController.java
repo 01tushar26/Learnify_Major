4c7d1d7bd47207728b5ae122152a8b145a3b9773
Tushar Tyagi <tushartyagi2601@gmail.com>
2026-03-30 23:24:57 +0530

    fix: cors error

--- server/src/main/java/com/LearnifyMajor/server/Controller/ChatController.java
+++ server/src/main/java/com/LearnifyMajor/server/Controller/ChatController.java
@@ -11,6 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor

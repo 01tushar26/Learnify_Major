6d2be3942d81033d66e1dab1a56c7ebaa2c29a92
Tushar Tyagi <tushartyagi2601@gmail.com>
2026-05-28 02:18:55 +0530

    fix : version issue

--- server/src/main/java/com/LearnifyMajor/server/Advice/GlobalResponseHandler.java
+++ server/src/main/java/com/LearnifyMajor/server/Advice/GlobalResponseHandler.java
@@ -1,6 +1,6 @@
 package com.LearnifyMajor.server.Advice;
 
-import org.jspecify.annotations.Nullable;
+
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
--- server/src/main/java/com/LearnifyMajor/server/Entity/Video.java
+++ server/src/main/java/com/LearnifyMajor/server/Entity/Video.java
@@ -1,14 +1,14 @@
-package com.LearnifyMajor.server.Entity;
-
-import jakarta.persistence.Entity;
-import lombok.*;
-
-@Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-public class Video {
-
-}
+//package com.LearnifyMajor.server.Entity;
+//
+//import jakarta.persistence.Entity;
+//import lombok.*;
+//
+//@Entity
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Builder
+//public class Video {
+//
+//}

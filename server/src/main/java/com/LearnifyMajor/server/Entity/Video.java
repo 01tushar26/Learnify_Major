9d234c952838b81620240f460a396d6873f0ff70
Tushar Tyagi <tushartyagi2601@gmail.com>
2026-05-27 23:50:59 +0530

    feat : create the entity for keep track of asyn video transcript

---
+++ server/src/main/java/com/LearnifyMajor/server/Entity/Video.java
@@ -0,0 +1,14 @@
+package com.LearnifyMajor.server.Entity;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Video {
+
+}

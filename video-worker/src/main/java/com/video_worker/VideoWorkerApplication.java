bce7442a12d06bad361b27da9bca129963d7891d
Tushar Tyagi <tushartyagi2601@gmail.com>
2026-09-09 14:15:06 +0530

    init : setup the video worker that consume the video queue

---
+++ video-worker/src/main/java/com/video_worker/VideoWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.video_worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoWorkerApplication.class, args);
+	}
+
+}
